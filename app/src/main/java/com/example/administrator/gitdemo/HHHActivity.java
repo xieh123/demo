567f337b7610ba57c14cb88ad8cb502b6f8c6bd9
xie_H <1509145359@qq.com>
2017-01-03 19:29:12 +0800

    adjust HHHActivity 111

--- app/src/main/java/com/example/administrator/gitdemo/HHHActivity.java
+++ app/src/main/java/com/example/administrator/gitdemo/HHHActivity.java
@@ -12,7 +12,7 @@ public class HHHActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main88);
+        setContentView(R.layout.main111);
 
     }
 }
