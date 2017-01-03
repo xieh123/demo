6223d9154cd807bf32fe8f82fcf81a8f8e2e1027
xie_H <1509145359@qq.com>
2017-01-03 18:52:29 +0800

    change HHHActivity.class add R.layout.view

--- app/src/main/java/com/example/administrator/gitdemo/HHHActivity.java
+++ app/src/main/java/com/example/administrator/gitdemo/HHHActivity.java
@@ -12,5 +12,7 @@ public class HHHActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.view);
+
     }
 }
